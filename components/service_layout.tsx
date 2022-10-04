0b9824be8bdbddc5f3a45a40bb3075ca5e01e303
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-04 13:17:39 +0900

    feat: login, logout created

--- components/service_layout.tsx
+++ components/service_layout.tsx
@@ -1,19 +1,26 @@
+import { Box, BoxProps } from "@chakra-ui/react";
 import Head from "next/head";
 import React from "react";
+import NavBar from "./NavBar";
 
 interface ServiceLayoutProps {
   title: string;
   children: React.ReactNode;
 }
 
-const ServiceLayout = ({ title = "blah x2", children }: ServiceLayoutProps) => {
+const ServiceLayout: React.FC<ServiceLayoutProps & BoxProps> = ({
+  title = "blah x2",
+  children,
+  ...boxProps
+}) => {
   return (
-    <div>
+    <Box {...boxProps}>
       <Head>
         <title>{title}</title>
       </Head>
+      <NavBar />
       {children}
-    </div>
+    </Box>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,20 @@ import { NextPage } from "next";
 import ServiceLayout from "../components/service_layout";
 import { Box, Center, Flex, Heading } from "@chakra-ui/react";
 import GoogleLoginButton from "../components/google_login_button";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import FirebaseClient from "../models/firebase_client";
-
-const provider = new GoogleAuthProvider();
+import { useAuth } from "../contexts/auth_user.context";
 
 const IndexPage: NextPage = function () {
+  const { signInWithGoogle } = useAuth();
   return (
-    <ServiceLayout title="test">
-      <Box maxW="md" mx="auto">
+    <ServiceLayout title="test" backgroundColor="gray.50" minH="100vh">
+      <Box maxW="md" mx="auto" pt="10">
         <img src="/main_logo.svg" alt="main logo" />
         <Flex justify="center">
           <Heading>#BlahBlah</Heading>
         </Flex>
       </Box>
       <Center mt="20">
-        <GoogleLoginButton
-          onClick={() => {
-            signInWithPopup(FirebaseClient.getInstance().Auth, provider)
-              .then((result) => {
-                console.info(result.user);
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-          }}
-        />
+        <GoogleLoginButton onClick={signInWithGoogle} />
       </Center>
     </ServiceLayout>
   );
